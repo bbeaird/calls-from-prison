c82ef68f22f39128bf0ec3e735a04a8d1644a5a2
Brantley Beaird <bbeaird@gmail.com>
2014-06-19 19:25:06 -0700

    Added an end to a block.

--- prison.rb
+++ prison.rb
@@ -10,6 +10,7 @@ get '/receive-call-from-prankster' do
   Twilio::TwiML::Response.new do |r|
     r.Gather numDigits: 10, action: 'receive-call-from-prankster/handle-gather', method: 'get' do |g|
       g.Say "Greetings! Let's tell family or a friend that you're in jail. Please enter the recipient's phone number"
+    end
   end.text
 end
 
